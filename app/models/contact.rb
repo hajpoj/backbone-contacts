db3738573b31fce3db5414c5467a08c8a2cfe57d
Hajime Furukawa <hajpoj@gmail.com>
2012-10-01 15:13:19 -0700

    add seed data to the app

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,4 @@
 class Contact < ActiveRecord::Base
   attr_accessible :address, :email, :name, :photo, :tel, :type
+  self.inheritance_column = 'inheritance_type'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+records = JSON.parse(File.read('db/contacts.json'))
+records.each do |record|
+  Contact.create!(record)
+end
