fb29dd75c743f1f1524b20b09010aabc8f2d1380
Hajime Furukawa <hajpoj@gmail.com>
2012-10-01 13:01:18 -0700

    AUTO GENERATED FILES ONLY - rails new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackboneContacts::Application.config.secret_token = '19fd8bf30ecd8a4960c9604024794a363660a39efc496015e4b1338f361e3b7b5a2344c222f6bce034dcedbb8f6e7fd452abe3423afedec0730be85df1426438'
